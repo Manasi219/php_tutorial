841b45303e57528e45cee3370d044e4bbbacd2db
Manasi219 <manasip995@gmail.com>
2020-07-29 20:25:58 +0530

    vi

---
+++ math_function.php
@@ -0,0 +1,18 @@
+<?php
+
+echo abs(-5.89) ."<br>";     //returns absolute positive value of number
+echo acos(0.47) ."<br>";     //returns arc cosine of a number
+echo acos(-1) ."<br>";
+echo acosh(1.22)."<br>";    // returns the inverse hyperbolic cosine of a number.
+
+echo sqrt(9)."<br>";               //returns square root of number
+
+echo max(2,89,45,34,101). "<br>";  //returns max value
+echo min(45,22,34,96) ."<br>";     //returns min value
+
+echo pow(11,2)."<br>";     // returns x raised to the power of y
+
+echo rand(10,100) ."<br>";  //generates a random integer
+
+echo round(-2.45)."<br>";                    //rounds a floating-point number
+?>
